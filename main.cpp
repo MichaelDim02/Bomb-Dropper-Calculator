1028cb9c34aac586eb97cff63a2bcddffbe187ee
Michael Constantine Dimopoulos <31562759+MichaelDim02@users.noreply.github.com>
2019-01-20 09:59:58 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,10 +15,9 @@ float distance;
 float atmo_pres;
 
 int main(){
-	std::cout << "BDC - Bomb Dropper Calculator v0.1" << std::endl << "By MD, MCD - Thessaloniki, Greece Oct. 2018" << std::endl;
+	std::cout << "BDC - Bomb Dropper Calculator v0.2" << std::endl << "By MD, MCD - Thessaloniki, Greece Oct. 2018" << std::endl;
 	get_data();
 	calculations();
-	system("pause");
 	return 0;
 }
 
@@ -27,7 +26,7 @@ void get_data(){
 	char filename [30] = "values.txt";
 	std::ifstream filestream;
 	filestream.open(filename);
-	
+
 	if(!filestream.is_open()){
 		exit(EXIT_FAILURE);
 	}
@@ -36,36 +35,39 @@ void get_data(){
 	int vline = 0;
 	while(filestream.good()){
 		filestream >> value;
+		std::cout << std::endl;
 		if (vline == 0) {
-			std::cout << "Height = " << " m" << value << std::endl;
+			std::cout << "Height = " << value << " m" << std::endl;
 			height = value;
 		} else if (vline == 1) {
-			std::cout << "Horizontal Bomb Speed = " << " m/s" << value << std::endl;
+			std::cout << "Horizontal Bomb Speed = " << value << "m/s" << std::endl;
 			horizontal_bomb_speed = value;
 		} else if (vline == 2) {
-			std::cout << "Airplane speed = " << "m/s" << value << std::endl;
-			airplane_speed = value;	
+			std::cout << "Airplane speed = " << value << "m/s" << std::endl;
+			airplane_speed = value;
 		} else if (vline == 3) {
-			std::cout << "Distance = " << " m" << value <<std::endl;
+			std::cout << "Distance = " << value << " m" << std::endl;
 			distance = value;
 		} else if (vline == 4) {
-			std::cout << "Atmospheric pressure = " << "m/s^2" << value << std::endl;
+			std::cout << "Atmospheric pressure = " << value << "m/s^2" << std::endl;
 			atmo_pres = value;
 		}
 		vline = vline + 1;
-	} 
- 
+	}
+
 }
 void calculations() {
 	std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
 	std::cout << "[-] Assuming target remains in the same position;" << std::endl;
 	std::cout << "[-] Assuming airplane is heading towards the target's position;" << std::endl;
 	float t = sqrt(2*height) / sqrt(atmo_pres);
+	horizontal_bomb_speed = horizontal_bomb_speed + airplane_speed;
 	float s2 = horizontal_bomb_speed * t;
 	float s1 = distance - s2;
-	std::cout << s1 << "m" << std::endl;
+	std::cout << "[+] Distance: " << s1 << "m" << std::endl;
 	float t1 = s1 / airplane_speed;
 	std::cout << "[!] Explosives will be dropped in " << t1 << "seconds. " << std::endl;
+	std::cout << "[-] Please, wait . . ." << std::endl;
 	float split = t1 / 4;
 	sleep(split);
 	std::cout << "[!] 3" << std::endl;
@@ -76,4 +78,3 @@ void calculations() {
 	sleep(split);
 	std::cout << "[!] Explosives dropped!" << std::endl;
 }
-
